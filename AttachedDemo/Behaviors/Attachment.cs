aa63c381c31f09e72e463697154f30f74e3ffdc1
Gidi Baum <gidibaum@netvision.net.il>
2016-11-23 18:18:55 +0200

    AttachedDemo

---
+++ AttachedDemo/Behaviors/Attachment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace AttachedDemo.Behaviors
+{
+    public class Attachment : DependencyObject
+    {
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.RegisterAttached("Color", typeof(Color), typeof(Attachment),
+                new UIPropertyMetadata(Colors.Black, (s, e) =>
+                {
+                    if (e.NewValue == null) return;
+
+                    var color = (Color)e.NewValue;
+
+                    var ctrl = s as FrameworkElement;
+                    if (ctrl == null)
+                        throw new InvalidOperationException("This attached property only supports types derived from FrameworkElement.");
+
+                    TextBlock.SetForeground(ctrl, new SolidColorBrush(color));
+                }));
+
+
+        public static Color GetColor(DependencyObject obj) => (Color)obj.GetValue(ColorProperty);
+       
+        public static void SetColor(DependencyObject obj, Color value) => obj.SetValue(ColorProperty, value);
+
+    }
+}
---
+++ AttachedDemo/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AttachedDemo.Views
+{
+ 
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
